b7f2bcfdb0aac08a35d9fd844b2d719da0eed615
BLB Avishka <buddhimaliyanageba@gmail.com>
2025-11-06 07:17:47 +0530

    Initialize project and create splash page

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+// Packages
+import 'package:firebase_core/firebase_core.dart';
+
+// Services
+import './services/navigation_service.dart';
+
+// Pages
+import './pages/splash_page.dart';
+
+
+void main() {
+  runApp(
+    SplashPage(key: UniqueKey(),
+    onInitializationComplete: () {
+      runApp(
+        MainApp()
+      );
+    }
+    )
+  );
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cipherly',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromRGBO(36, 35, 49, 1.0),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Color.fromRGBO(30, 29, 37, 1.0)
+        )
+      ),
+      // navigatorKey: NavigationService.navigatorKey,
+    );
+  }
+}
+
